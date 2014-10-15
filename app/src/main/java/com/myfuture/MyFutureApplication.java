8227991c9351d5ac6a7af8ce4464aee6f4bf8162
Andrew Rodriguez <a.rodriguez97@umiami.edu>
2014-10-14 22:43:54 -0400

    first commit

---
+++ app/src/main/java/com/myfuture/MyFutureApplication.java
@@ -0,0 +1,92 @@
+package com.myfuture;
+
+
+import com.myfuture.model.VideoPostModel;
+
+
+import  com.myfuture.R;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.PushService;
+import com.parse.ParseAnalytics;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import android.app.Application;
+import android.util.Log;
+
+public class MyFutureApplication extends Application {
+    private final static String TAG = MyFutureApplication.class.getSimpleName();
+    public static final boolean APPDEBUG = false;
+
+    // Debugging tag for the application
+    public static final String APPTAG = "Video Map";
+    // Key for saving the search distance preference
+    private static final String KEY_SEARCH_DISTANCE = "searchDistance";
+
+    private static SharedPreferences preferences;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG,"onCreate");
+
+        /*
+         * In this tutorial, we'll subclass ParseObject for convenience to
+         * create and modify Meal objects
+         */
+        ParseObject.registerSubclass(VideoPostModel.class);
+        preferences = getSharedPreferences("com.myfuture", Context.MODE_PRIVATE);
+
+        /*
+         * Fill in this section with your Parse credentials
+         */
+        Parse.initialize(this, "D79Ngo6Zi9SJriRhq3dnQumbkB1T5BPUQKmLS8P9", "XIjEMLvQRCKX5K1HRAuIreI0x9CW94w5WT8ZVztS");
+        //ParseFacebookUtils.initialize(getString(R.string.facebook_id));
+
+        /*
+         * This app lets an anonymous user create and save photos of meals
+         * they've eaten. An anonymous user is a user that can be created
+         * without a username and password but still has all of the same
+         * capabilities as any other ParseUser.
+         *
+         * After logging out, an anonymous user is abandoned, and its data is no
+         * longer accessible. In your own app, you can convert anonymous users
+         * to regular users so that data persists.
+         *
+         * Learn more about the ParseUser class:
+         * https://www.parse.com/docs/android_guide#users
+         */
+        ParseUser.enableAutomaticUser();
+
+        /*
+         * For more information on app security and Parse ACL:
+         * https://www.parse.com/docs/android_guide#security-recommendations
+         */
+        ParseACL defaultACL = new ParseACL();
+
+        /*
+         * If you would like all objects to be private by default, remove this
+         * line
+         */
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+
+
+    }
+
+    public static float getSearchDistance() {
+        return 250; //TODO: fix this
+    }
+
+    public static void setSearchDistance(float value) {
+        preferences.edit().putFloat(KEY_SEARCH_DISTANCE, value).commit();
+    }
+
+}
---
+++ app/src/main/java/com/myfuture/model/VideoPostModel.java
@@ -0,0 +1,67 @@
+package com.myfuture.model;
+
+import java.util.Date;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+/*
+ * An extension of ParseObject that makes
+ * it more convenient to access information
+ * about a given shot
+ */
+
+@ParseClassName("VideoPostModel")
+public class VideoPostModel extends ParseObject {
+
+    public VideoPostModel() {
+        // A default constructor is required.
+    }
+
+
+    public ParseUser getAuthor() {
+        return getParseUser("user");
+    }
+
+    public void setAuthor(ParseUser user) {
+        put("user", user);
+    }
+
+    public void setTitle(String points) {
+        put("title", points);
+    }
+
+    public String getTitle() {
+        return this.getString("title");
+    }
+
+    //for gps
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint("location");
+    }
+
+    public void setLocation(ParseGeoPoint value) {
+        put("location", value);
+    }
+
+    public String getDescription() {
+        return this.getString("description");
+    }
+
+    public void setDescription(String description) {
+        put("description", description);
+    }
+
+    public String getUrl() {
+        return getString("url");
+    }
+
+    public void setUrl(String url) {
+        put("url", url);
+    }
+}
